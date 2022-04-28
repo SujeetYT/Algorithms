49c71d61ebf564cf82b8a345f5a846b15ca79146
SujeetYT <sujeet7982422191@gmail.com>
2022-04-28 18:42:10 +0530

    Added binary search

---
+++ binarySearchAlgorithm.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int* arr, int size, int key){
+    int low = 0, high = size-1;
+    int mid = (low+high)/2;
+    while(low <= high){
+        if(key == arr[mid])
+            return mid;
+        
+        if(key >= arr[mid])
+            low = mid+1;
+        else high = mid-1;
+
+        mid = (low+high)/2;
+    } 
+    return -1;
+}
+
+int main(){
+    int totalElements;
+    cin>>totalElements;
+
+    int* arr = new int[totalElements];
+    for(int i =0; i < totalElements; i++){
+        cin>>arr[i];
+    }
+
+    int key;
+    cin>>key;
+
+    cout<<binarySearch(arr, totalElements, key);
+    return 0;
+}
